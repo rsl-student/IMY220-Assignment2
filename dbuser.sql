ef4f8a2b932a03e4abdf30e3c78f59b80598a5b7
rsl-student <u15226532@tuks.co.za>
2020-08-28 01:23:41 +0200

    Add files via upload
    updated the dbusers files

--- dbuser.sql
+++ dbuser.sql
@@ -2,7 +2,7 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
+-- Host: 127.0.0.1
 -- Generation Time: Aug 27, 2020 at 06:51 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
@@ -10,7 +10,6 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
